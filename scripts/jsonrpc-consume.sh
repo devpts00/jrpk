8f8075477e3c1bf545a692973cbd4611081e8c3d
Andrey Kozyrev <andrey.ko3yrev@yandex.ru>
2025-12-31 18:08:26 +0300

    cadvisor container to trak cpu and memory

--- scripts/jsonrpc-consume.sh
+++ scripts/jsonrpc-consume.sh
@@ -7,28 +7,29 @@ export PARTITIONS=$4
 
 START_TIME=$EPOCHREALTIME
 
-for ((p = 0; p < $PARTITIONS; p++))
+for ((i = 0; i < 100; i++))
 do
-	./target/${BUILD}/jrpk \
-		client \
-		--path=./json/${FILE}-${p}.json \
-		--address=jrpk:1133 \
-		--topic=posts \
-		--partition=${p} \
-		--metrics-uri=http://pmg:9091/metrics/job/jrpk \
-		--metrics-period=1s \
-		consume \
-		--from=earliest \
-		--until=latest \
-		--max-batch-byte-size=128KiB \
-		--max-wait-ms=50 &
-	sleep 0.1
+  for ((p = 0; p < $PARTITIONS; p++))
+  do
+    ./target/${BUILD}/jrpk \
+      client \
+      --path=./json/${FILE}-${p}.json \
+      --address=jrpk:1133 \
+      --topic=posts \
+      --partition=${p} \
+      --metrics-uri=http://pmg:9091/metrics/job/jrpk \
+      --metrics-period=1s \
+      consume \
+      --from=earliest \
+      --until=latest \
+      --max-batch-byte-size=128KiB \
+      --max-wait-ms=50 &
+    sleep 0.1
+  done
+  wait
+  rm -f ./json/${FILE}-*.json
 done
-
-wait
 
 END_TIME=$EPOCHREALTIME
 DIFF_TIME=$(echo "$END_TIME - $START_TIME" | bc)
 echo "Runtime: $DIFF_TIME seconds"
-
-rm -f ./json/${FILE}-*.json
